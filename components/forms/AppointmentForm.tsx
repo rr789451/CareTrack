c75f25f4525c9b2587e4d51d4040b934c359c72f
Rohit <rr789451@gmail.com>
2025-01-05 03:53:58 +0530

    feat: Milestone 6 - Admin Dashboard and Logic

--- components/forms/AppointmentForm.tsx
+++ components/forms/AppointmentForm.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-non-null-asserted-optional-chain */
 "use client"
  
 import { zodResolver } from "@hookform/resolvers/zod"
@@ -13,16 +14,21 @@ import { FormFieldType } from "./PatientForm"
 import { SelectItem } from "../ui/select"
 import { Doctors } from "@/constants"
 import Image from "next/image"
-import { createAppointment } from "@/lib/actions/appointment.actions"
+import { createAppointment, updateAppointment } from "@/lib/actions/appointment.actions"
+import { Appointment } from "@/types/appwrite.types"
 
 const AppointmentForm = ({
     userId,
     patientId,
-    type
+    type,
+    appointment,
+    setOpen,
 }:{
-    userId: string,
-    patientId: string,
-    type: "create" | "cancel" | "schedule"
+    userId: string;
+    patientId: string;
+    type: "create" | "cancel" | "schedule";
+    appointment?: Appointment;
+    setOpen: (open: boolean) => void;
 } ) => {
 
   const [isLoading, setIsLoading] = useState(false);
@@ -33,17 +39,16 @@ const AppointmentForm = ({
   const form = useForm<z.infer<typeof AppointmentFormValidation>>({
     resolver: zodResolver(AppointmentFormValidation),
     defaultValues: {
-      primaryPhysician: "",
-      schedule: new Date(),
-      reason: "",
-      note: "",
-      cancellationReason: ""
+      primaryPhysician: appointment ? appointment.primaryPhysician : '',
+      schedule: appointment ? new Date(appointment?.schedule) : new Date(Date.now()),
+      reason: appointment ? appointment.reason : '',
+      note: appointment?.note || '',
+      cancellationReason: appointment?.cancellationReason || '',
     },
   })
 
   async function onSubmit(values: z.infer<typeof AppointmentFormValidation>) {
     setIsLoading(true);
-    console.log('HERE')
     let status;
 
     switch (type){
@@ -59,8 +64,6 @@ const AppointmentForm = ({
             status = 'pending';
             break;
       }
-
-      console.log("Before Try", type);
 
     try {
         if(type === 'create' && patientId){
@@ -79,6 +82,25 @@ const AppointmentForm = ({
             if(appointment){
                 form.reset();
                 router.push(`/patients/${userId}/new-appointment/success?appointmentId=${appointment.$id}`)
+            }
+        } else {
+            const appointmentToUpdate = {
+                userId,
+                appointmentId: appointment?.$id!,
+                appointment:{
+                    primaryPhysician: values?.primaryPhysician,
+                    schedule: new Date(values?.schedule),
+                    status: status as Status,
+                    cancellationReason: values?.cancellationReason,
+                },
+                type
+            }
+
+            const updatedAppointment = await updateAppointment(appointmentToUpdate);
+
+            if(updatedAppointment) {
+                setOpen && setOpen(false);
+                form.reset();
             }
         }
     } catch (error) {
@@ -107,10 +129,10 @@ const AppointmentForm = ({
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 flex-1">
-        <section className="mb-12 space-y-4">
+        {type === 'create' && <section className="mb-12 space-y-4">
             <h1 className="header">Hey there 👋</h1>
             <p className="text-dark-700">Request a new appointment in 10 seconds.</p>
-        </section>
+        </section>}
 
         {type !== "cancel" && (
             <>
@@ -162,7 +184,7 @@ const AppointmentForm = ({
                         fieldType={FormFieldType.TEXTAREA}
                         control={form.control}
                         name="note"
-                        label="Additional Comments/notes"
+                        label="Notes"
                         placeholder="ex: Prefer afternoon appointments, if possible"
                     />
                 </div>
